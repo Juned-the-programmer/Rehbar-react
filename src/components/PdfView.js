d83751f1ab86eafb76b8ab008fc557f5312c554d
Juned-the-programmer <davdajuned7698@gmail.com>
2022-03-13 18:06:59 +0530

    set loading flag

--- src/components/PdfView.js
+++ src/components/PdfView.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import HTMLFlipBook from "react-pageflip";
 import { pdfjs, Document, Page as ReactPdfPage } from "react-pdf";
 import PDF from "../assets/DreamsInIslam.pdf";
@@ -6,6 +6,7 @@ import useWindowDimensions from "./GetView";
 
 const PdfView = (props) => {
   // condition for select pdf
+  const [numPages, setNumPages] = useState(null);
   const { height, width } = useWindowDimensions();
 
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -22,14 +23,29 @@ const PdfView = (props) => {
     );
   });
 
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   const arr = [];
-  for (let i = 1; i <= 105; i++) {
+  for (let i = 1; i <= Number(numPages); i++) {
     arr.push(i);
   }
 
+  const loadingFlag = () => {
+    return <h3 className="text-center text-muted mt-5">Loading...</h3>;
+  };
+
   return (
     <React.Fragment>
-      <Document file={PDF}>
+      <Document
+        file={PDF}
+        onLoadSuccess={onDocumentLoadSuccess}
+        loading={loadingFlag}
+        onLoadError={(error) =>
+          alert("Error while retrieving the outline! " + error.message)
+        }
+      >
         <HTMLFlipBook width={width} height={height}>
           {arr?.map((pages) => {
             return <Page key={pages} width={width} pageNumber={pages} />;
