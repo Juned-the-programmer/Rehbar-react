fb9a3c2c8bd6563b23a2b92d83331466979db3d7
Juned-the-programmer <davdajuned7698@gmail.com>
2022-03-19 23:55:12 +0530

    PDF

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,14 @@ import VOL1 from "../assets/vol-1/Vol-1 feb 2022.pdf"
 import VOL1IMG from "../assets/vol-1/Vol-1.jpg"
 import VOL2 from "../assets/Vol-2/1.pdf"
 import VOL2IMG from "../assets/Vol-2/Deep_Learning.png"
+import VOL3 from "../assets/Vol-3/3.pdf"
+import VOL3IMG from "../assets/Vol-3/vol-3.jpg"
+import VOL4 from "../assets/Vol-4/4.pdf"
+import VOL4IMG from "../assets/Vol-4/vol-4.jpg"
+import VOL5 from "../assets/Vol-5/5.pdf"
+import VOL5IMG from "../assets/Vol-5/vol-5.jpg"
+import VOL6 from "../assets/Vol-6/6.pdf"
+import VOL6IMG from "../assets/Vol-6/vol-6.png"
 
 const Home = ({ setPdf, setBackIcon, AD, setAD }) => {
   const history = useHistory();
@@ -44,6 +52,50 @@ const Home = ({ setPdf, setBackIcon, AD, setAD }) => {
                   style={{ width: "inherit", height: "250px" }}
                   onClick={(e) => {
                     setPdf(VOL2);
+                    history.push("/pdf");
+                  }}
+                />
+              </Col>
+              <Col md="3" sm="6" xs="6" className="mt-3">
+                <img
+                  src={VOL3IMG}
+                  name="PIC_1"
+                  style={{ width: "inherit", height: "250px" }}
+                  onClick={(e) => {
+                    setPdf(VOL3);
+                    history.push("/pdf");
+                  }}
+                />
+              </Col>
+              <Col md="3" sm="6" xs="6" className="mt-3">
+                <img
+                  src={VOL4IMG}
+                  name="PIC_1"
+                  style={{ width: "inherit", height: "250px" }}
+                  onClick={(e) => {
+                    setPdf(VOL4);
+                    history.push("/pdf");
+                  }}
+                />
+              </Col>
+              <Col md="3" sm="6" xs="6" className="mt-3 mb-5">
+                <img
+                  src={VOL5IMG}
+                  name="PIC_1"
+                  style={{ width: "inherit", height: "250px" }}
+                  onClick={(e) => {
+                    setPdf(VOL5);
+                    history.push("/pdf");
+                  }}
+                />
+              </Col>
+              <Col md="3" sm="6" xs="6" className="mt-3 mb-5">
+                <img
+                  src={VOL6IMG}
+                  name="PIC_1"
+                  style={{ width: "inherit", height: "250px" }}
+                  onClick={(e) => {
+                    setPdf(VOL6);
                     history.push("/pdf");
                   }}
                 />
